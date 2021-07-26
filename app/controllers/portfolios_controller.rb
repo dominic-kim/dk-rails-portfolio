7597680e33614d4d9657e64279d1af417c32aa9b
Dom Kim <domkim@gmail.com>
2021-07-26 12:45:57 -0400

    implemented edit and udpate functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,23 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
