49d366cfe26e67a45d38a4c618095337e6741403
Dom Kim <domkim@gmail.com>
2021-07-31 13:43:52 -0400

    added guest user for current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -58,6 +58,8 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  private
+
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
